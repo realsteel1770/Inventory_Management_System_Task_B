dcbceb7f536ae3db432609f93fe087c1aba40b1f
Leo Steel <leosteel@Leos-MacBook-Air.local>
2024-11-12 14:59:41 +0000

    partically working code

--- sales.py
+++ sales.py
@@ -1,23 +1,15 @@
 import random
+import csv 
 
 def daily_sales(available_items, inventory_records, current_day):
-    '''
-***********COMPLETE THIS FUNCTION***********
-This function is responsible for updating the sales for a given day.
----------------
-Function Input:
----------------
-available_items: (integer) Available Tshirts from the previous day.
-inventory_records: (List) A list of inventory records until the previous day. Each row contains (day, sales, restocked items, available items)
-current_day: (integer) Day number which you want to add as the current day. 
+    if current_day % 7 == 0:
+        sold_units = 0  
+        available_items = 2000  
+    else:
+        sold_units = random.randint(0, 200)
+        sold_units = min(sold_units, available_items)  
+        available_items -= sold_units  
+    available_items = max(0, available_items)
+    inventory_records.append([current_day, sold_units, 0, available_items])
 
-----------------
-Function Output:
-----------------
-available_items:(integer) This function returns this integer which updates the available items at the current day.
-
-The function will also update the inventory_records (For restocking) for a  given current day. 
-
-    '''
-    
     return available_items
