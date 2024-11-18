09d49f2b41a13eab35b6e1d900e2f37f134ff9cf
Leo Steel <leosteel@Leos-MacBook-Air.local>
2024-11-18 12:45:28 +0000

    error correcting

--- restock.py
+++ restock.py
@@ -1,11 +1,16 @@
 import csv
 
 def restock_inventory(available_items, inventory_records, current_day):
-    
-    if current_day % 7 == 0:  
-        restock_inventory = 2000 - available_items  
-        available_items = 2000  
-        inventory_records.append([current_day, 0, restock_inventory, available_items])  
+    sold_units = 0 
+    if current_day == 0:
+        restock_amount = 2000
+        available_items = 2000
+        inventory_records.append([current_day, sold_units, restock_amount, available_items])
+        
+    elif current_day % 7 == 0: 
+        restock_amount = 2000 - available_items
+        available_items += restock_amount
+        inventory_records.append([current_day, sold_units, restock_amount, available_items, ])  
 
     return available_items
 
--- sales.py
+++ sales.py
@@ -2,14 +2,10 @@ import random
 import csv 
 
 def daily_sales(available_items, inventory_records, current_day):
-    if current_day % 7 == 0:
-        sold_units = 0  
-        available_items = 2000  
-    else:
-        sold_units = random.randint(0, 200)
-        sold_units = min(sold_units, available_items)  
+    restocked_amount = 0 
+    sold_units = random.randint(0, 200)
+    if current_day % 7 != 0:
         available_items -= sold_units  
-    available_items = max(0, available_items)
-    inventory_records.append([current_day, sold_units, 0, available_items])
+        inventory_records.append([current_day, sold_units,restocked_amount, available_items])
 
     return available_items
